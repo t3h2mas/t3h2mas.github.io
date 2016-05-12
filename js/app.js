e5cf3217ce07c2855fa7498260d21afa8c6e133a
Thomas <jtruant@gmx.com>
2016-05-12 03:15:58 -0600

    animate

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('#mail').on('click touchstart', () => {
+  $('#mail').on('click', function {
     $('#hide').toggleClass('hidden');
+    $('html, body').animate({ scrollTop: $(document).height() }, 'fast');
   });
 });
