bffb0843a9fae1ae82811040840639bfb6031470
Thomas <jtruant@gmx.com>
2016-05-12 18:56:52 -0600

    mobile scroll fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('#mail').on('click', function () {
     $('#hide').toggleClass('hidden');
+    $('body').css('overflow-y', 'hidden');
     $('body').stop().animate({ scrollTop: $(document).height() - $(window).height() }, 300);
+    $('body').css('overflow-y', 'auto');
   });
 });
